645cf9dcb9f16f21ad0abaa95741bdc6171aaf16
pxz2011 <113100518+pxz2011@users.noreply.github.com>
2023-07-13 09:56:37 +0800

    master

--- src/views/index.vue
+++ src/views/index.vue
@@ -6,16 +6,63 @@
         @click="logout">登出
     </el-button>
   </div>
+  <el-pagination v-model:currentPage="currentPage" :page-sizes="[5,10,15,20]"
+                 layout=" prev, pager, next, jumper,total, sizes," :total="400" @size-change="handlerSizeChange"
+                 @current-change="handleCurrentChange"/>
+  <el-table :data="this.tableData" style="width: 100%" max-height="250">
+    <el-table-column fixed prop="time" label="时间" width="150"/>
+    <el-table-column prop="thing" label="计划" width="120"/>
+    <el-table-column prop="place" label="地点" width="120"/>
+    <el-table-column fixed="right" label="Operations" width="120">
+      <template #default="scope">
+        <el-button
+            link
+            type="primary"
+            size="small"
+            @click.prevent="deleteRow(scope.$index)"
+        >
+          Remove
+        </el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+  <el-button class="mt-4" style="width: 100%" @click="onAddItem"
+  >Add Item
+  </el-button
+  >
+
 </template>
 
-<script>
+<script lang="js">
 import axios from "axios";
 import {ElMessage} from "element-plus";
-import router from "@/router";
+import router from "../router/index";
 
 export default {
+
   name: "index",
+  data: function () {
+    return {
+      pageNum: 1,
+      pageSize: 5,
+      tableData: []
+    }
+  },
+  mounted() {
+    this.loadPage(this.pageNum,this.pageSize)
+  },
   methods: {
+    handlerSizeChange: function (val) {
+      console.log("每页数量:$val")
+      this.pageSize = val
+      this.loadPage(this.pageNum, this.pageSize)
+    },
+    handleCurrentChange: function (val) {
+      console.log(val)
+      this.pageNum = val;
+      this.loadPage(this.pageNum, this.pageSize)
+
+    },
     list: function () {
       axios({
         method: 'get',
@@ -48,9 +95,24 @@ export default {
           ElMessage.error(res.data.msg)
         }
       })
+    },
+    loadPage(pageNum, pageSize) {
+      let url = 'http://localhost:8080/plan/page';
+      axios.get(url, {
+        params: {
+          // pageNum:this.pageNum,
+          // pageSize:this.pageSize
+          pageNum,
+          pageSize
+        },
+        headers: {
+          'token': localStorage.getItem('token')
+        }
+      }).then(resp => {
+        console.log(resp)
+      });
     }
   }
-
 }
 </script>
 
