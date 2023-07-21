6b6e5fa60a88f3e31f1be320d160ff031fc4b1a9
pxz2011 <113100518+pxz2011@users.noreply.github.com>
2023-07-21 15:34:13 +0800

    Revert "变更"
    This reverts commit cda5d2a664ff7f5e3bdbba929a55806abf94ffab.

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,8 @@ import {createRouter, createWebHashHistory} from 'vue-router'
 const routes = [
     {
         path: "/login",
-        name: "login",
-        component: () => import("@/views/login.vue")
+        name: "Login",
+        component: import("@/views/login")
     },
     {
         path: "/",
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,12 @@
-const {defineConfig} = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-    transpileDependencies: true,
-    devServer: {
-        port: 9090,
-    },
-    //解决build打包的时候 dist文件里面css、js、img路径错误错误的问题
-    publicPath: './',
-    // 输出文件目录
-    outputDir: 'dist',
+  transpileDependencies: true,
+  devServer: {
+    port: 9090,
+  },
+  //解决build打包的时候 dist文件里面css、js、img路径错误错误的问题
+  publicPath: './',
+  outputDir: "dist",
+  assetsDir: 'public'
+
 })
